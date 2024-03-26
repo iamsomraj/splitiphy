54a0bd9aabfdff47f1025522b98fd284ad8264d9
Somraj Mukherjee <iamsomraj@gmail.com>
2024-03-26 21:33:36 +0530

    👌 IMPROVE: Update database schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -21,9 +21,33 @@ export const groupMemberships = pgTable('group_memberships', {
 
 export const transactions = pgTable('transactions', {
   id: serial('id').primaryKey(),
-  description: text('description').notNull(),
   amount: decimal('amount', { precision: 10, scale: 2 }).notNull(),
-  transaction_date: date('transaction_date').notNull(),
+  ownerId: text('owner_id').notNull(),
+  payerId: text('payer_id').notNull(),
+  receiverId: text('receiver_id').notNull(),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at'),
+  isDeleted: boolean('is_deleted').notNull().default(false),
+});
+
+export const expenses = pgTable('expenses', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+  description: text('description').notNull(),
+  expenseDate: date('expense_date').notNull(),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at'),
+  isDeleted: boolean('is_deleted').notNull().default(false),
+});
+
+export const expenseTransactions = pgTable('expense_transactions', {
+  id: serial('id').primaryKey(),
+  expenseId: integer('expense_id')
+    .references(() => expenses.id, { onDelete: 'cascade' })
+    .notNull(),
+  transactionId: integer('transaction_id')
+    .references(() => transactions.id, { onDelete: 'cascade' })
+    .notNull(),
   createdAt: timestamp('created_at').notNull(),
   updatedAt: timestamp('updated_at'),
   isDeleted: boolean('is_deleted').notNull().default(false),
@@ -32,29 +56,25 @@ export const transactions = pgTable('transactions', {
 export const groupExpenses = pgTable('group_expenses', {
   id: serial('id').primaryKey(),
   expenseId: integer('expense_id')
-    .references(() => transactions.id, { onDelete: 'cascade' })
+    .references(() => expenses.id, { onDelete: 'cascade' })
     .notNull(),
   groupId: integer('group_id')
     .references(() => groups.id, { onDelete: 'cascade' })
     .notNull(),
-  payerId: text('payer_id').notNull(),
-  receiverId: text('receiver_id').notNull(),
   createdAt: timestamp('created_at').notNull(),
   updatedAt: timestamp('updated_at'),
   isDeleted: boolean('is_deleted').notNull().default(false),
 });
 
-export const groupUserBalances = pgTable('group_user_balances', {
+export const userBalances = pgTable('user_balances', {
   id: serial('id').primaryKey(),
   groupId: integer('group_id')
     .references(() => groups.id, { onDelete: 'cascade' })
     .notNull(),
   userId: text('user_id').notNull(),
   balance: decimal('balance', { precision: 10, scale: 2 }).notNull(),
-  lastTransactionId: integer('last_transaction_id')
-    .references(() => transactions.id)
-    .notNull(),
-  lastTransactionDate: date('last_transaction_date').notNull(),
+  lastExpenseId: integer('last_expense_id').references(() => expenses.id, { onDelete: 'cascade' }),
+  lastExpenseDate: date('last_expense_date'),
   createdAt: timestamp('created_at').notNull(),
   updatedAt: timestamp('updated_at'),
   isDeleted: boolean('is_deleted').notNull().default(false),
