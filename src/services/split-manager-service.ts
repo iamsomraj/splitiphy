03dae4c8c48f174714879004c8a65b9ab2f4e877
Somraj Mukherjee <iamsomraj@gmail.com>
2024-04-01 22:37:05 +0530

    👌 IMPROVE: Add split manager service

---
+++ src/services/split-manager-service.ts
@@ -0,0 +1,106 @@
+interface Edge {
+  node: string;
+  weight: number;
+}
+
+interface Transaction {
+  payer: string;
+  receiver: string;
+  amount: number;
+}
+
+interface Balances {
+  [key: string]: number;
+}
+
+class DirectedGraph {
+  adjList: { [key: string]: Edge[] };
+
+  constructor() {
+    this.adjList = {};
+  }
+
+  addVertex(vertex: string): void {
+    if (!this.adjList[vertex]) {
+      this.adjList[vertex] = [];
+    }
+  }
+
+  addEdge(source: string, destination: string, weight: number): void {
+    this.addVertex(source);
+    this.addVertex(destination);
+    this.adjList[source].push({ node: destination, weight: weight });
+  }
+
+  getBalances(): Balances {
+    let balances: Balances = {};
+
+    for (let source in this.adjList) {
+      if (!balances[source]) {
+        balances[source] = 0;
+      }
+
+      for (let edge of this.adjList[source]) {
+        let destination = edge.node;
+        let weight = edge.weight;
+
+        if (!balances[destination]) {
+          balances[destination] = 0;
+        }
+
+        balances[source] -= weight;
+        balances[destination] += weight;
+      }
+    }
+
+    return balances;
+  }
+}
+
+class SplitManagerService {
+  transactions: Transaction[];
+  graph: DirectedGraph;
+
+  constructor(transactions: Transaction[]) {
+    this.transactions = transactions;
+    this.graph = new DirectedGraph();
+    this.setGraphEdges();
+  }
+
+  setGraphEdges(): void {
+    this.transactions.forEach((transaction) => {
+      const { payer, receiver, amount } = transaction;
+      this.graph.addEdge(payer, receiver, amount);
+    });
+  }
+
+  settleBalances(): Transaction[] {
+    let balances = this.graph.getBalances();
+    let settlement = true;
+    let settlements: Transaction[] = [];
+
+    for (let source in balances) {
+      for (let destination in balances) {
+        if (balances[source] < 0 && balances[destination] > 0) {
+          const amount = Math.min(-balances[source], balances[destination]);
+          balances[source] += amount;
+          balances[destination] -= amount;
+          settlements.push({
+            payer: source,
+            receiver: destination,
+            amount,
+          });
+          settlement = false;
+        }
+      }
+    }
+
+    if (settlement) {
+      return [];
+    }
+
+    return settlements;
+  }
+}
+
+export default SplitManagerService;
