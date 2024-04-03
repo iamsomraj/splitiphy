781aedb3ba8b66436958539db8dfdef39f4ad648
Somraj Mukherjee <iamsomraj@gmail.com>
2024-04-03 22:21:10 +0530

    👌 IMPROVE: add simplify button

--- src/components/groups/group-simplify-form.tsx
+++ src/components/groups/group-simplify-form.tsx
@@ -3,26 +3,20 @@
 import * as actions from '@/actions';
 import FormButton from '@/components/shared/form-button';
 import { GroupWithData } from '@/db/queries';
-import { useFormState } from 'react-dom';
 
 type GroupMembersProps = {
   group: GroupWithData;
 };
 
 const GroupSimplifyForm = ({ group }: GroupMembersProps) => {
-  const [formState, action] = useFormState(
-    actions.simplifyGroupExpenses.bind(null, group?.uuid || ''),
-    {
-      errors: {},
-    },
-  );
-  return group ? (
-    <form action={action}>
-      {formState?.errors?._form ? (
-        <div>{formState.errors._form?.join(', ')}</div>
-      ) : null}
-      <FormButton>Settle Up</FormButton>
-    </form>
+  return group && group.uuid ? (
+    <div>
+      <FormButton
+        onClick={async () => actions.simplifyGroupExpenses(group?.uuid || '')}
+      >
+        Simplify Expenses
+      </FormButton>
+    </div>
   ) : null;
 };
 
