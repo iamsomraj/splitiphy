901f18db9afcde096b1ede2c3e5b0f60d360710c
Somraj Mukherjee <iamsomraj@gmail.com>
2024-04-20 15:21:09 +0530

    👌 IMPROVE: Update group user search page layout and components

--- src/app/(main)/groups/[uuid]/search/page.tsx
+++ src/app/(main)/groups/[uuid]/search/page.tsx
@@ -15,23 +15,32 @@ type GroupUserSearchPageProps = {
   };
 };
 
+const BackButton = ({ groupUuid }: { groupUuid: string }) => (
+  <Link href={paths.groupShow(groupUuid)}>
+    <Button variant="outline" size="icon" className="h-7 w-7">
+      <ChevronLeft className="h-4 w-4" />
+      <span className="sr-only">Back</span>
+    </Button>
+  </Link>
+);
+
+const GroupTitle = ({ groupName }: { groupName: string }) => (
+  <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
+    Add Users to {groupName}
+  </h1>
+);
+
 const GroupUserSearchPage = async ({
   params,
   searchParams,
 }: GroupUserSearchPageProps) => {
   const group = await getGroupDetailsById(params.uuid);
+
   return (
     <main className="flex flex-1 flex-col gap-6 p-4 pb-8 pt-6 sm:p-6 lg:p-12 lg:pb-16">
       <div className="flex items-center justify-start gap-4">
-        <Link href={paths.groupShow(group?.uuid || '')}>
-          <Button variant="outline" size="icon" className="h-7 w-7">
-            <ChevronLeft className="h-4 w-4" />
-            <span className="sr-only">Back</span>
-          </Button>
-        </Link>
-        <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
-          Add Users to {group?.name}
-        </h1>
+        <BackButton groupUuid={group?.uuid || ''} />
+        <GroupTitle groupName={group?.name || ''} />
       </div>
       <GroupUserSearchForm group={group} />
       <UserList
