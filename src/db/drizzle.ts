724ed746aad2199380d7fc637bdf8a260ff0bcc9
Somraj Mukherjee <iamsomraj@gmail.com>
2024-04-21 10:57:24 +0530

    👌 IMPROVE: Add env validation

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,8 +1,9 @@
 import * as schema from '@/db/schema';
+import { env } from '@/env';
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(env.DATABASE_URL);
 const db = drizzle(sql, { schema });
 
 export default db;
