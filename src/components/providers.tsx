b7a4250d231dc5faea5fba4b535671fe68593e0d
Somraj Mukherjee <iamsomraj@gmail.com>
2024-05-10 23:16:00 +0530

    feat: Add nextjs-toploader package for improved page loading

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,10 +4,12 @@ import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
+import NextTopLoader from 'nextjs-toploader';
 
 const Providers = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <>
+      <NextTopLoader />
       <ClerkProvider
         appearance={{
           baseTheme: dark,
