c36c3d5f33a3145706d44773e188e7071b7ccfa0
Somraj Mukherjee <iamsomraj@gmail.com>
2024-05-11 22:39:31 +0530

    👌 IMPROVE:  Add db changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,13 @@ import type { Config } from 'drizzle-kit';
 export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    host: process.env.DB_HOST!,
+    port: +process.env.DB_PORT!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
+    ssl: true,
   },
 } satisfies Config;
--- src/components/groups/group-item.tsx
+++ src/components/groups/group-item.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import * as actions from '@/actions';
 import {
   Card,
   CardContent,
@@ -19,44 +20,62 @@ import paths from '@/lib/paths';
 import { DotsVerticalIcon } from '@radix-ui/react-icons';
 import { formatDistanceToNow } from 'date-fns';
 import Link from 'next/link';
+import { useTransition } from 'react';
+import { cn } from '@/lib/utils';
 
 type GroupItemProps = {
   group: ManyGroupsWithData[0];
 };
 
 const GroupItem = ({ group }: GroupItemProps) => {
+  const [pending, startTransition] = useTransition();
   return (
-    <Link href={paths.groupShow(group.uuid)}>
-      <Card className="hover:bg-accent">
-        <CardHeader>
-          <CardTitle className="flex justify-between">
+    <Card
+      className={cn(
+        'hover:bg-accent',
+        pending && 'pointer-events-none opacity-60',
+      )}
+    >
+      <CardHeader>
+        <CardTitle className="flex cursor-pointer justify-between">
+          <Link href={paths.groupShow(group.uuid)}>
             <span>{group.name}</span>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="h-8 w-8 p-0">
-                  <span className="sr-only">Open menu</span>
-                  <DotsVerticalIcon className="h-4 w-4" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={async () => {}}>
-                  Delete
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </CardTitle>
-          <CardDescription className="text-sm font-semibold text-accent-foreground/40">
+          </Link>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <DotsVerticalIcon className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem
+                onClick={() => {
+                  startTransition(async () => {
+                    await actions.deleteGroup(group?.uuid || '');
+                  });
+                }}
+              >
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </CardTitle>
+        <CardDescription className="cursor-pointer text-sm font-semibold text-accent-foreground/40">
+          <Link href={paths.groupShow(group.uuid)}>
             {formatDistanceToNow(new Date(group.createdAt), {
               addSuffix: true,
             })}
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
+          </Link>
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="cursor-pointer">
+        <Link href={paths.groupShow(group.uuid)}>
           <p>{group.owner.firstName + ' ' + group.owner.lastName}</p>
           <span>{group.groupMemberships.length} members</span>
-        </CardContent>
-      </Card>
-    </Link>
+        </Link>
+      </CardContent>
+    </Card>
   );
 };
 
