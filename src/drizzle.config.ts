db79d4a1fcdb9abe4f89d145592ef21a59e7fa91
Somraj Mukherjee <iamsomraj@gmail.com>
2024-05-11 22:49:50 +0530

    👌 IMPROVE: Move db config to src folder

--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -1,16 +1,16 @@
-import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
+import { env } from '@/env';
 
 export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    port: +process.env.DB_PORT!,
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_NAME!,
+    host: env.DB_HOST,
+    port: +env.DB_PORT!,
+    user: env.DB_USER!,
+    password: env.DB_PASSWORD!,
+    database: env.DB_NAME!,
     ssl: true,
   },
 } satisfies Config;
