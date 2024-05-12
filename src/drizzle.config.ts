f086cfdacf78215367b7289a1d2cfb68e4c86ea5
Somraj Mukherjee <iamsomraj@gmail.com>
2024-05-12 11:43:57 +0530

    refactor: Update database configuration to use URL instead of individual credentials

--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -6,11 +6,6 @@ export default {
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    host: env.DB_HOST,
-    port: +env.DB_PORT!,
-    user: env.DB_USER!,
-    password: env.DB_PASSWORD!,
-    database: env.DB_NAME!,
-    ssl: true,
+    url: env.DATABASE_URL,
   },
 } satisfies Config;
